cdc100efaf4f061c7ed1b27c22a2d862a97f02ad
Stefano Torresi <stefano@torresi.io>
2016-01-19 21:17:07 +0100

    add php xdebug aliases

--- roles/zsh/files/aliases.zsh
+++ roles/zsh/files/aliases.zsh
@@ -1,6 +1,8 @@
 alias l='ls -lh'
 alias ll='ls -lAh'
 alias la='ls -laF'
+alias php-debug='php -dzend_extension=xdebug.so'
+alias phpunit-debug="php-debug $(which phpunit)"
 unalias _
 unalias c
 unalias git
